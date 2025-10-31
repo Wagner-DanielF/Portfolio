50823f332c605296e982bae3fbdd555cbd3786ae
Wagner Daniel Felix <wadafe@gmail.com>
2025-10-30 23:25:18 -0300

    mais ajuste do responsivo
    alguns ajuste do responsivo e efetuado a formatação do código

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function enviarWhats(event) {
     event.preventDefault();
-    
+
     const nome = document.getElementById('nome').value;
     const mensagem = document.getElementById('mensagem').value;
     const telefone = '5511973342574'
@@ -10,18 +10,18 @@ function enviarWhats(event) {
 
     const url = `https://wa.me/${telefone}?text=${msgFormatada}`
     //const url = `https://api.whatsapp.com/${telefone}=${msgFormatada}`
-    
+
 
     window.open(url, '_blank')
 }
 
-(function(){
+(function () {
     emailjs.init("J2VcYKcc1RsqfQ02s");
 })();
 
 function enviarEmail(event) {
     event.preventDefault();
-    
+
     const nome = document.getElementById('nome').value;
     const mensagem = document.getElementById('mensagem').value;
 
@@ -30,13 +30,13 @@ function enviarEmail(event) {
         message: mensagem,
         to_email: 'wdf_sb@hotmail.com'
     })
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-       alert('Email enviado com sucesso!');
-    }, function(error) {
-       console.log('FAILED...', error);
-       alert('Falha ao enviar o email.');
-    });
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Email enviado com sucesso!');
+        }, function (error) {
+            console.log('FAILED...', error);
+            alert('Falha ao enviar o email.');
+        });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
